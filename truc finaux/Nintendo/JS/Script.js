10190e9c1bc8a609858f2f759b09751eb1d90436
Naruto2983 <49168631+Naruto2983@users.noreply.github.com>
2019-05-22 13:06:46 -0400

    Add files via upload

---
+++ truc finaux/Nintendo/JS/Script.js
@@ -0,0 +1,21 @@
+$('#33').click(function () {
+   $('#bowser').fadeOut()
+});
+
+
+$('#button').click(function () {
+    var nomImage = $('#bowser')
+
+    if(nomImage == $('#bowser'))
+    {
+        $('#bowser').fadeIn()
+    }
+
+    else
+    {
+        $('#bowser').fadeOut()
+    }
+});
+
+$('#gos').html("Les <strong>jeux Nintendo</strong> ne sont pas là sans les bons vieux critiqueurs de jeux video");
+$('#gos').css('color','gold');
